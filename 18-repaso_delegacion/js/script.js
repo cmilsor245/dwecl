d6e0a5529920ea41ca52be1ad60fa4159d09d069
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-07 17:47:30 +0100

    commit

--- 18-repaso_delegacion/js/script.js
+++ 18-repaso_delegacion/js/script.js
@@ -1,11 +1,29 @@
-const contenedorCardsDestinos = document.querySelector("section.destinos .contenedor-cards");
-const contenedorCardsHospedaje = document.querySelector("section.hospedaje .contenedor-cards");
-const componenteCardsHospedaje = document.querySelectorAll("section.hospedaje .contenedor-cards .card");
+const DESTINOS_CARDS_COMPONENTES = document.querySelector("section.destinos .contenedor-cards");
 
-contenedorCardsDestinos.addEventListener("click", (event) => {
+/* ------------------------------------------------------------------------------------------------- */
+
+console.log(DESTINOS_CARDS_COMPONENTES.children[0].children[0].getAttribute("img-id"));
+
+/* ------------------------------------------------------------------------------------------------- */
+
+DESTINOS_CARDS_COMPONENTES.addEventListener("click", (event) => {
   console.log(`coordenada x: ${event.clientX}`);
   console.log(`coordenada y: ${event.clientY}`);
   console.log(`botón: ${event.buttons}`);
   console.log(`componente: ${event.target}`);
-  console.log(`contenido: ${event.target.textContent}`);
-})
+});
+
+/* ------------------------------------------------------------------------------------------------- */
+
+DESTINOS_CARDS_COMPONENTES.addEventListener("click", (event) => {
+  if (event.target.tagName === "P") {
+    showTitleContent(event);
+  }
+});
+
+function showTitleContent(event) {
+  alert(event.target.textContent);
+}
+
+/* ------------------------------------------------------------------------------------------------- */
+
