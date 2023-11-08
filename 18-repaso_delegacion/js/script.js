0f971624eeef0ff0cb84f39fdf3d46db72d62640
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-08 17:36:52 +0100

    commit

--- 18-repaso_delegacion/js/script.js
+++ 18-repaso_delegacion/js/script.js
@@ -1,25 +1,26 @@
-const DESTINOS_CARDS_COMPONENTES = document.querySelector("section.destinos .contenedor-cards");
+const DESTINOS_CARDS_COMPONENT = document.querySelector("section.destinos .contenedor-cards");
+const WHAT_TO_DO_NY_COMPONENT = document.querySelectorAll("main.contenido");
 
 /* ------------------------------------------------------------------------------------------------- */
 
-console.log(DESTINOS_CARDS_COMPONENTES.children[0].children[0].getAttribute("img-id"));
+console.log(DESTINOS_CARDS_COMPONENT.children[0].children[0].getAttribute("img-id"));
 
 /* ------------------------------------------------------------------------------------------------- */
 
-DESTINOS_CARDS_COMPONENTES.addEventListener("click", (event) => {
+DESTINOS_CARDS_COMPONENT.addEventListener("click", (event) => {
   console.log(`coordenada x: ${event.clientX}`);
   console.log(`coordenada y: ${event.clientY}`);
   console.log(`botón: ${event.buttons}`);
   console.log(`componente: ${event.target}`);
-});
+})
 
 /* ------------------------------------------------------------------------------------------------- */
 
-DESTINOS_CARDS_COMPONENTES.addEventListener("click", (event) => {
+DESTINOS_CARDS_COMPONENT.addEventListener("click", (event) => {
   if (event.target.tagName === "P") {
     showTitleContent(event);
   }
-});
+})
 
 function showTitleContent(event) {
   alert(event.target.textContent);
@@ -27,3 +28,8 @@ function showTitleContent(event) {
 
 /* ------------------------------------------------------------------------------------------------- */
 
+WHAT_TO_DO_NY_COMPONENT.addEventListener("click", whatToDoNYTitle);
+
+function whatToDoNYTitle(event) {
+  console.log("testing");
+}
