468f669673a657195b3134e0601117f955a9a514
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-10 12:37:36 +0100

    commit

--- 20-subir_de_nota-boletin_de_notas/v1/js/script.js
+++ 20-subir_de_nota-boletin_de_notas/v1/js/script.js
@@ -1,9 +1,9 @@
 const STUDENTS_LIST = [
-  { name: "christian", grades: [7.5, 8.2, 6.8, 9.1, 7.9, 8.6, 7.3, 8.9, 6.5, 7.8] },
-  { name: "rocío", grades: [6.4, 7.9, 8.1, 7.2, 6.9, 7.8, 8.3, 7.6, 6.7, 7.5] },
-  { name: "óscar", grades: [8.9, 7.6, 9.2, 8.4, 7.8, 8.5, 9.1, 7.9, 8.2, 9.0] },
-  { name: "víctor", grades: [7.2, 6.8, 7.9, 6.5, 7.1, 6.9, 7.8, 6.7, 7.3, 6.6] },
-  { name: "safaa", grades: [9.5, 8.8, 9.2, 9.7, 8.9, 9.4, 9.1, 8.7, 9.3, 8.5] }
+  { name: "christian", grades: [7.5, 8.2, 6.8, 9.1, 3.9, 8.6, 7.3, 8.9, 6.5, 6.8, 4.0, 3.5] },
+  { name: "rocío", grades: [6.4, 7.9, 1.1, 7.2, 3.9, 7.8, 8.3, 7.6, 6.7, 4.5, 4.5, 4.4] },
+  { name: "óscar", grades: [7.9, 7.6, 9.2, 2.4, 7.8, 8.5, 0.1, 7.9, 8.2, 9.0, 3.8, 4.3] },
+  { name: "víctor", grades: [7.2, 6.8, 7.9, 6.5, 7.1, 2.9, 7.8, 6.7, 4.3, 6.6, 4.2, 3.7] },
+  { name: "safaa", grades: [1.5, 8.8, 9.2, 9.7, 8.9, 9.4, 9.1, 8.7, 9.3, 8.5, 4.1, 4.0] }
 ];
 
 const STUDENT_NAME = document.getElementById("student-name");
@@ -15,13 +15,13 @@ CHECK_BUTTON.addEventListener("click", (e) => {
   if (checkEmptyInput()) {
     alert("por favor, introduce un nombre válido");
   } else {
-    makeCheckButtonDisappear();
+    removeCheckButton();
     createGradeList();
   }
 });
 
-function makeCheckButtonDisappear() {
-  CHECK_BUTTON.classList.add("hidden-check-button");
+function removeCheckButton() {
+  CHECK_BUTTON.remove();
 }
 
 function checkEmptyInput() {
@@ -30,15 +30,20 @@ function checkEmptyInput() {
 
 function createGradeList() {
   const GRADES_HEADER = document.createElement("h4");
-  GRADES_HEADER.textContent = "notas";
+  GRADES_HEADER.textContent = "notas:";
   GRADES_HEADER.id = "grades-header";
+  GRADES_HEADER.classList.add("grades-container");
 
   const GRADES_LIST = document.createElement("ul");
+  GRADES_LIST.id = "grades-list";
+  GRADES_LIST.classList.add("grades-container");
+
   STUDENTS_LIST.forEach((student) => {
     if (student.name.toLowerCase() === STUDENT_NAME.value.toLowerCase()) {
       student.grades.forEach((grade) => {
         const GRADE_ITEM = document.createElement("li");
         GRADE_ITEM.textContent = grade;
+        GRADE_ITEM.classList.add("grade-item");
         GRADES_LIST.appendChild(GRADE_ITEM);
       });
     }
