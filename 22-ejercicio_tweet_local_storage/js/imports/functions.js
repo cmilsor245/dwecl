3c014422cc44c8c1786728826866a0263260e3c7
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-19 22:30:20 +0100

    commit

--- 22-ejercicio_tweet_local_storage/js/imports/functions.js
+++ 22-ejercicio_tweet_local_storage/js/imports/functions.js
@@ -1,13 +1,24 @@
 export function uploadTweet(tweet_input, tweets_array, trash_icon_svg, tweet_list_element) {
   const TWEET = tweet_input.value;
 
-  clearInput(tweet_input);
+  if (!checkEmptyInput(tweet_input)) {
+    clearInput(tweet_input);
 
-  pushTweetToArray(TWEET, tweets_array);
+    pushTweetToArray(TWEET, tweets_array);
 
-  updateLocalStorage(tweets_array);
+    updateLocalStorage(tweets_array);
 
-  appendTweetToList(tweets_array, trash_icon_svg, tweet_list_element);  
+    appendTweetToList(tweets_array[tweets_array.length - 1], trash_icon_svg, tweet_list_element);
+  }
+}
+
+function checkEmptyInput(tweet_input) {
+  if (tweet_input.value === "") {
+    alert("Por favor, introduce un tweet");
+    tweet_input.focus();
+    return true;
+  }
+  return false;
 }
 
 function clearInput(input) {
@@ -26,17 +37,17 @@ function pushTweetToArray(tweet_text, tweets_array) {
 }
 
 function updateLocalStorage(tweets_array) {
-  localStorage.setItem("tweets", JSON.stringify(tweets_array));
+  localStorage.setItem('tweets', JSON.stringify(tweets_array));
 }
 
-function appendTweetToList(tweets_array, trash_icon_svg, tweet_list_element) {
-  const NEW_TWEET = document.createElement("li");
-  NEW_TWEET.innerHTML = `<span id = "tweet-li-text">${tweets_array[tweets_array.length - 1].text}</span><span id = "trash-icon">${trash_icon_svg}</span>`;
+export function appendTweetToList(tweet, trash_icon_svg, tweet_list_element) {
+  const NEW_TWEET = document.createElement('li');
+  NEW_TWEET.innerHTML = `<span id="tweet-li-text">${tweet.text}</span><span id="trash-icon">${trash_icon_svg}</span>`;
   tweet_list_element.appendChild(NEW_TWEET);
 }
 
 export function handleTweetListWrapperClick(event, tweet_input) {
-  if (event.target.closest(".trash-icon-svg")) { // he tenido que utilizar "closest" por utilizar una imagen dentro del span
+  if (event.target.closest(".trash-icon-svg")) {
     const TWEET = event.target.closest("li");
     TWEET.remove();
     alert("Tweet eliminado");
