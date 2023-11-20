f46cd01149e82087fe94daa448d2a2ed53a6c9c9
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-20 12:26:36 +0100

    commit

--- 22-local_storage/ejercicio_tweet_local_storage/v2-correct_use/js/script.js
+++ 22-local_storage/ejercicio_tweet_local_storage/v2-correct_use/js/script.js
@@ -26,12 +26,13 @@ CLEAR_TWEETS_BUTTON.addEventListener("click", () => {
 })
 
 TWEET_LIST_WRAPPER.addEventListener("click", (event) => {
-  /* const CLICKED_TRASH_ICON = event.target.closest("#trash-icon-span"); // si se hace clic en cualquier elemento cuyo padre sea #trash-icon-span se completa el evento
+  const CLICKED_TRASH_ICON = event.target.closest("#trash-icon-span"); // si se hace clic en cualquier elemento cuyo padre sea #trash-icon-span se completa el evento
   if (CLICKED_TRASH_ICON) {
     console.log(`test ${Date.now()}`);
-  } */
-
-  if (event.target.closest("#trash-icon-span")) {
-    
   }
+
+  /* const CLICKED_TRASH_ICON = event.target.closest("#trash-icon-svg");
+  if (CLICKED_TRASH_ICON) {
+    
+  } */
 })
