f67ca035e1ac74867bb7f7b6b14540493f664777
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-20 01:04:59 +0100

    commit

--- 22-local_storage/ejercicio_tweet_local_storage/v2-correct_use/js/script.js
+++ 22-local_storage/ejercicio_tweet_local_storage/v2-correct_use/js/script.js
@@ -1,1 +1,7 @@
+import * as functions from "./imports/functions.js";
+
 const TWEET_INPUT = document.querySelector("#tweet-input");
+const UPLOAD_TWEET_BUTTON = document.querySelector("#upload-tweet-button");
+
+const TWEET_LIST_ELEMENT = document.querySelector("#tweet-list-element");
+
