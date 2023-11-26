e2b63349d1652c7d15f3d12cc5cb4bb04e195174
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-26 22:45:03 +0100

    commit

---
+++ 23-promesas/01-ejercicio_promesas/js/script.js
@@ -0,0 +1,5 @@
+const TXT_BUTTON = document.querySelector("#txt-button");
+const OBJECT_BUTTON = document.querySelector("#object-button");
+const ARRAY_BUTTON = document.querySelector("#array-button");
+const API_BUTTON = document.querySelector("#api-button");
+
