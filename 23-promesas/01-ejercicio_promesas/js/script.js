6ba8fea7127ffd25f5d5956b905482342f9eb048
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-26 23:03:07 +0100

    commit

--- 23-promesas/01-ejercicio_promesas/js/script.js
+++ 23-promesas/01-ejercicio_promesas/js/script.js
@@ -8,53 +8,31 @@ BUTTONS_CONTAINER.addEventListener("click", (event) => {
     obtainTXT();
   } else if (BUTTON.id === "object-button") {
     obtainJSON();
-  } else if (BUTTON.id === "array-button") {
-    obtainArray();
-  } else if (BUTTON.id === "api-button") {
+  } else if (BUTTON.id === "array-button" || BUTTON.id === "api-button") {
     obtainAPI();
   }
-})
+});
 
 async function obtainTXT() {
   const DATA = await fetch("data/data.txt");
-
   const DATA_TEXT = await DATA.text();
-
-  console.log(DATA_TEXT);
-
-  displaResult(DATA_TEXT);
+  displayResult(DATA_TEXT);
 }
 
 async function obtainJSON() {
-  const DATA = await fetch("data/employees.json");
-
+  const DATA = await fetch("data/employee.json");
   const DATA_JSON = await DATA.json();
-
-  console.log(DATA_JSON);
-
-  displaResult(DATA_JSON);
-}
-
-async function obtainArray() {
-  const DATA = await fetch("data/employees.json");
-
-  const DATA_JSON = await DATA.json();
-
-  console.log(DATA_JSON);
-
-  displayResult(DATA_JSON);
+  const dataString = JSON.stringify(DATA_JSON, null, 2);
+  displayResult(dataString);
 }
 
 async function obtainAPI() {
   const DATA = await fetch("data/employees.json");
-
   const DATA_JSON = await DATA.json();
-
-  console.log(DATA_JSON);
-
-  displayResult(DATA_JSON);
+  const dataString = JSON.stringify(DATA_JSON, null, 2);
+  displayResult(dataString);
 }
 
-function displaResult(result) {
+function displayResult(result) {
   CONTENT.innerHTML = result;
 }
