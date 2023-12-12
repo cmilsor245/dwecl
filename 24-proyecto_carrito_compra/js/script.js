f80425ceb6cec0cc7022ebfe4bfedf60bb973163
cmilsor245 <cmilsor245@g.educaand.es>
2023-12-12 13:13:32 +0100

    commit

--- 24-proyecto_carrito_compra/js/script.js
+++ 24-proyecto_carrito_compra/js/script.js
@@ -4,10 +4,23 @@ import * as functions from "./imports/functions.js"
 /* -------------------------------------------------------------------------------------------------------- */
 
 // hito 1 -> carga de dom
-const COURSES_LIST = document.querySelector("#lista-cursos")
+const CART_TABLE = document.querySelector("#lista-carrito tbody")
+const EMPTY_CART_BUTTON = document.querySelector("#vaciar-carrito")
+const COURSES_LIST_ELEMENT = document.querySelector("#lista-cursos")
 const JSON_PATH = "data/data.json"
 
 /* -------------------------------------------------------------------------------------------------------- */
 
 // hito 9 -> json - cards dinámicas
-functions.fetchData(JSON_PATH, COURSES_LIST)
+functions.fetchData(JSON_PATH, COURSES_LIST_ELEMENT)
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+// hito 2 -> event listeners
+COURSES_LIST_ELEMENT.addEventListener("click", (event) => {
+  functions.addToCart(event, CART_TABLE)
+})
+
+EMPTY_CART_BUTTON.addEventListener("click", () => {
+  functions.emptyCartDynamically(CART_TABLE)
+})
